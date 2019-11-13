3ee172d2c20bc8bff15473088e5b77e1a00ec3d4
mattshardman <mattshardman@gmail.com>
2019-11-13 13:28:54 +0000

    day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,59 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111 
+MUL = 0b10100010
+HLT = 0b00000001 
+
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.pc = 0
+        self.running = True
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+
+        self.branch_table = {
+            LDI: self.handle_ldi,
+            PRN: self.handle_prn,
+            MUL: self.handle_mul,
+            HLT: self.handle_hlt,
+        }
 
     def load(self):
         """Load a program into memory."""
-
+        f = open(sys.argv[1], "r")
         address = 0
+        for line in f:
+            ln = ''
+            for char in line:
+                if char == '#':
+                    break
+                if char == '\n':
+                    break
+                ln += char
+            if len(ln) > 0:
+                self.ram[address] = int(ln, 2)
+            
+        
 
-        # For now, we've just hardcoded a program:
+    def ram_read(self, MAR):
+        if MAR <= len(self.ram) - 1:
+            return self.ram[MAR]
+        return 0
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +66,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +78,27 @@ class CPU:
 
         print()
 
+    def handle_ldi(self, op_a, op_b):
+        self.reg[op_a] = op_b
+
+    def handle_prn(self, op_a, _op_a):
+        print(self.reg[op_a])
+
+    def handle_mul(self, op_a, op_b):
+        self.alu("MUL", op_a, op_b)
+
+    def handle_hlt(self, _op_a, _op_b):
+        self.running = False
+
     def run(self):
         """Run the CPU."""
-        pass
+        while self.running:
+        # set memory at pc counter to intruction register
+            ir = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            self.branch_table[ir](operand_a, operand_b)
+
+            self.pc += 1 + (ir >> 6)
+
